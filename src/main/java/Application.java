5b6a2a4be1adcb8ac13e2021a8594cdd23fdedb2
axoneuk <info@axone-tech.uk>
2019-12-01 08:39:27 +0000

    Added new sout statement

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,9 @@
 public class Application {
     public static void main(String[] args) {
         System.out.println("Project using Maven");
+
+        System.out.println("Project using JEnkins");
     }
+
 
 }
